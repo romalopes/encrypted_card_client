ba2b465d63bae80445e7d804495f4a104c62196e
romalopes <romalopes@yahoo.com.br>
2016-03-08 00:50:03 +1100

    database

--- environments.rb
+++ environments.rb
@@ -1,7 +1,5 @@
 require 'sinatra'
-# require 'sinatra/sequel'
 configure :development, :test do
- # set :database, 'sqlite:///dev.db'
  # set :database, 'sqlite3:name.db'
  # set :show_exceptions, true
 	 ActiveRecord::Base.establish_connection(
@@ -9,12 +7,3 @@ configure :development, :test do
 	  :database => 'name.db'
 	) 
 end
-
-
-
-# configure do
-# 	# DB = Sequel.connect('sqlite://database.db')
-# 	DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://database.db')
-
-# 	# DB setup here
-# end
